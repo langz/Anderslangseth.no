b79e542fecab3ed30822a7de44de5b37bd2ffcfa
langz <anderslangseth@hotmail.com>
2014-09-22 00:33:24 +0200

    Added angular to Skills

--- contact.php
+++ contact.php
@@ -1,44 +1,44 @@
-<?php 
+<?php
 
-	require_once('config.php');
+require_once('config.php');
 
-	// Sender Info
-	$name = trim($_POST['name']);
-	$email = trim($_POST['mail']);
-	$message = trim($_POST['comment']);
-	$err = "";
-	
-	// Check Info
-	$pattern = "^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$^";
-	if(!preg_match_all($pattern, $email, $out)) {
-		$err = MSG_INVALID_EMAIL; // Invalid email
-	}
-	if(!$email) {
-		$err = MSG_INVALID_EMAIL; // No Email
-	}	
-	if(!$message) {
-		$err = MSG_INVALID_MESSAGE; // No Message
-	}
-	if (!$name) {
-		$err = MSG_INVALID_NAME; // No name 
-	}
+// Sender Info
+$name = trim($_POST['name']);
+$email = trim($_POST['mail']);
+$message = trim($_POST['comment']);
+$err = "";
 
-	//define the headers we want passed. Note that they are separated with \r\n
-	$headers = "From: ".$name." <".$email.">\r\nReply-To: ".$email."";
+// Check Info
+$pattern = "^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$^";
+if(!preg_match_all($pattern, $email, $out)) {
+	$err = MSG_INVALID_EMAIL; // Invalid email
+}
+if(!$email) {
+	$err = MSG_INVALID_EMAIL; // No Email
+}
+if(!$message) {
+	$err = MSG_INVALID_MESSAGE; // No Message
+}
+if (!$name) {
+	$err = MSG_INVALID_NAME; // No name
+}
 
-	if (!$err){
-		
-		//send the email
-		$sent = mail(TO_EMAIL,SUBJECT,$message,$headers); 
-		
-		if ($sent) {
-				// If the message is sent successfully print
-				echo "Sendt"; 
-			} else {
-				// Display Error Message
-				echo MSG_SEND_ERROR; 
-			}
+//define the headers we want passed. Note that they are separated with \r\n
+$headers = "From: ".$name." <".$email.">\r\nReply-To: ".$email."";
+
+if (!$err){
+
+	//send the email
+	$sent = mail(TO_EMAIL,SUBJECT,$message,$headers);
+
+	if ($sent) {
+		// If the message is sent successfully print
+		echo "Sendt";
 	} else {
-		echo $err; // Display Error Message
+		// Display Error Message
+		echo MSG_SEND_ERROR;
 	}
+} else {
+	echo $err; // Display Error Message
+}
 ?>
